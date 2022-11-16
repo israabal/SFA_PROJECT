6974f74ae04fe507344121b1eb24e080c103294a
TasneemEsleem <8tasneem19988@gmail.com>
2022-11-16 11:57:32 +0200

    login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function showLogin(Request $request)
+    {
+        $request->merge(['guard' => $request->guard]);
+        $validator = Validator($request->all(), [
+            'guard' => 'required|string|in:admin,user,store'
+        ]);
+        session()->put('guard', $request->input('guard'));
+        if (!$validator->fails()) {
+            return response()->view('cms.auth.login');
+        } else {
+            abort(404);
+        }
+    }
+    public function login(Request $request)
+    {
+        $validator = Validator($request->all(), [
+            'email' => 'required|string',
+            'password' => 'required|string|min:3|max:10',
+            'remember' => 'required|boolean',
+        ]);
+
+        $guard = session('guard');
+        $credentials = ['email' => $request->get('email'), 'password' => $request->get('password')];
+        if (!$validator->fails()) {
+            if (Auth::guard($guard)->attempt($credentials, $request->get('remember'))) {
+                // Language::all();
+                return response()->json(['message' => 'Logged in successfully'], Response::HTTP_OK);
+            } else {
+                return response()->json(['message' => 'Error credentials, please try again'], Response::HTTP_BAD_REQUEST);
+            }
+        } else {
+            return response()->json(['message' => $validator->getMessageBag()->first()], Response::HTTP_BAD_REQUEST);
+        }
+    }
+    public function logout(Request $request)
+    {
+        $guard = session('guard');
+        Auth::guard($guard)->logout();
+        $request->session()->invalidate();
+        return redirect()->route('cms.login', $guard);
+    }
+   }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ResetPasswordController;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 /*
@@ -20,26 +22,26 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 
 
-
 Route::group(
     [
         'prefix' => LaravelLocalization::setLocale(),
-        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
-    ], function(){ //...
-
-
-
-
-
-
-
-        Route::get('/', function () {
-            return view('cms.parent');
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+    ],
+    function () { //...
+        Route::prefix('cms')->middleware('guest:admin')->group(function () {
+            Route::get('{guard}/login', [AuthController::class, 'showLogin'])->name('cms.login');
+            Route::post('/login', [AuthController::class, 'login']);
         });
+        Route::post('login', [AuthController::class, 'login']);
+        Route::middleware('guest')->group(function () {
+            Route::get('/forgot-password', [ResetPasswordController::class, 'requestPasswordReset'])->name('password.request');
+            Route::post('/forgot-password', [ResetPasswordController::class, 'sendResetEmail'])->name('password.email');
+            Route::get('/reset-password/{token}', [ResetPasswordController::class, 'resetPassword'])->name('password.reset');
+            Route::post('/reset-password', [ResetPasswordController::class, 'updatePassword'])->name('password.update');
+        });
+    }
+);
 
-
-
-
-
-    });
-
+Route::get('test', function () {
+    return view('cms.parent');
+});
